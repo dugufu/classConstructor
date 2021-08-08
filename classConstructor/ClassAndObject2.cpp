e69fb0c32ff5f64895064600779ce2aa8232369b
ccr <cchunren1997@gmail.com>
2021-08-08 21:18:31 +0800

    2021 08 08

---
+++ classConstructor/ClassAndObject2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+using namespace std;
+// 
+// class PL {
+// public:
+// 	PL(int damage, int hp, int armor):m_damage(damage), m_hp(hp), m_armor(armor) // 0 = real, 1 = illusion
+// 	{
+// 		cout << "PL status : Real one" << endl;
+// 		cout << "PL damage : \t"<< m_damage << endl;
+// 		cout << "PL hp : \t" << m_hp << endl;
+// 		cout << "PL armor : \t" << m_armor << endl;
+// 	}
+// 
+// 	PL(const PL& pl)
+// 	{
+// 		m_damage = pl.m_damage / 2;
+// 		m_hp = pl.m_hp / 2;
+// 		m_armor = pl.m_armor / 2;
+// 		cout << "PL status : Illusion" << endl;
+// 		cout << "PL damage : \t" << m_damage << endl;
+// 		cout << "PL hp : \t" << m_hp << endl;
+// 		cout << "PL armor : \t" << m_armor << endl;
+// 	}
+// 
+// 	float m_damage;
+// 	float m_hp;
+// 	float m_armor;
+// };
+// 
+// void createActorPL() {
+// 	PL pl(8, 40, 5);
+// 	PL pl2(pl);
+// 
+// };
+// 
+// 
+// int main() {
+// 	createActorPL();
+// 
+// }
--- classConstructor/classAndObject.cpp
+++ classConstructor/classAndObject.cpp
@@ -1,3 +1,67 @@
 #include <iostream>
 #include <string>
 using namespace std;
+// 
+// class Animal {
+// public:
+// 
+// 	int age;
+// 
+// };
+// 
+// class Sheet : virtual public Animal {};
+// 
+// class Camel : virtual public Animal{};
+// 
+// class SheetCamel : public Sheet, public Camel
+// {
+// };
+// class Animal {
+// public:
+// 	virtual void doSpeak() {
+// 		cout << "Animal speaking" << endl;
+// 	}
+// 
+// };
+// 
+// class Cat : public Animal {
+// 	void doSpeak() {
+// 		cout << "Meow ~" << endl;
+// 	}
+// };
+// 
+// class Dog : public Animal{
+// 	void doSpeak() {
+// 		cout << "wlof !" << endl;
+// 	};
+// };
+// 
+// class Horse : public Animal {
+// 	void doSpeak() {
+// 		cout << "MIeeeeeee !" << endl;
+// 	};
+// };
+// 
+// 
+// void AnimalSpeak(Animal &animal) {
+// 	animal.doSpeak();
+// }
+// int main() {
+// 	Cat cat;
+// 	Dog dog;
+// 	Horse horse;
+// 	AnimalSpeak(cat);
+// 	AnimalSpeak(dog);
+// 	AnimalSpeak(horse);
+
+	//cout << sizeof(Animal) << endl;
+// 	SheetCamel sc;
+// 	sc.Sheet::age = 10;
+// 	sc.Camel::age = 20;
+// 
+// 	cout << "sc.Sheet::age :" << sc.Sheet::age << endl;
+// 	cout << "sc.Camel::age :" << sc.Camel::age << endl;
+// 
+// 	cout << sizeof(sc);
+	
+//}
