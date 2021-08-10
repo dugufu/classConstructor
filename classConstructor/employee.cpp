df0acaadbf57832265b9c8792f19a11448b7fce4
ccr <cchunren1997@gmail.com>
2021-08-11 02:13:30 +0800

    updated add, display, delete,edit, search employee functionality 2.13am  2021 08 11

--- classConstructor/employee.cpp
+++ classConstructor/employee.cpp
@@ -22,33 +22,32 @@ void showMenu() {
 int main() {
 	int num = -1;
 	int program = -1;
-	int checkIntType; // return 1 = true 0 = false
+	Employee_level emp;
 	while (program != 0)
 	{
-		jump_to_showMenu:
 		system("CLS");
 		showMenu();
 		cout << "\nEnter Number here" << endl;
-		cin >> num;
-		checkIntType = checkVarIsInt(&num); // return 1 = true 0 = false
-		if (checkIntType != 1)
-		{
-			goto jump_to_showMenu;
-		}
+		checkIntInputType(&num);
 		switch (num)
 		{
 		case 0:
-			ExitProgram(&program);
+			ExitProgram(&program);// Exit program
 			break;
 		case 1:
+			AddNewEmployee(&emp);
 			break;
 		case 2:
+			DisplayDetails(&emp);
 			break;
 		case 3:
+			DeleteEmployee(&emp);
 			break;
 		case 4:
+			EditEmployeeDetails(&emp);
 			break;
 		case 5:
+			SearchEmployee(&emp);
 			break;
 		case 6:
 			break;
@@ -64,16 +63,200 @@ int main() {
 void ExitProgram(int * num) {
 	*num = 0;
 }
+//Add
+void AddNewEmployee(Employee_level * emp_lv) {
+	jump_to_add_employee:
+	cout << "How many employees are u gonna sign?" << endl;
+	int num;
+	checkIntInputType(&num);
 
-
-//checking num 
-int checkVarIsInt(int* numInt) {
-
-	if (*numInt != -1)
+	if (num > 0)
 	{
-		return 1;
+		string e_name;
+		int position;
+		for (int i = 0; i < num; i++)
+		{
+			jump_name:
+			cout << "Enter Employee " << i + 1 << " name :" << endl;
+			cin >> e_name;
+			if (e_name != "")
+			{
+				cout << "Enter Employee " << i + 1 << " position : ( 0 = Boss 1 = Manager 2 = Employee )" << endl;
+				checkIntInputType(&position);
+				emp_lv->personArray[emp_lv->m_employee_id].m_name = e_name;
+				emp_lv->personArray[emp_lv->m_employee_id].m_position = position;
+				emp_lv->m_employee_id++;
+
+			}
+			else {
+				goto jump_name;
+			}
+		}
 	}
 	else {
-		return 0;
+		goto jump_to_add_employee;
+	}
+
+}
+
+//display
+void DisplayDetails(Employee_level * emp_lvl) {
+	if (emp_lvl->m_employee_id > 0)
+	{
+		for (int i = 0; i < emp_lvl->m_employee_id ; i++)
+		{
+			Boss* temp_person = EnterPosition(&emp_lvl->personArray[i].m_position);
+			cout<< "Employee ID :"<< i + 1 << "\tName : " << emp_lvl->personArray[i].m_name << "\t\tPosition : ";
+			temp_person->duty();
+			cout << endl;
+			delete temp_person;
+			temp_person = NULL;
+
+		}
+	}
+	else {
+		cout << "No data found" << endl;
+
+	}
+	system("pause");
+}
+
+
+//search
+void SearchEmployee(Employee_level* emp_lvl, int deleteStatus) {
+	string status_string;
+	if (deleteStatus == 1) { status_string = "delete"; }
+	else { status_string = "search"; };
+	
+	cout << "Select one of these options to "<< status_string <<" the data you want" << endl;
+	cout << "Option 1 : "<< status_string <<" by employee ID press keyboard 1" << endl;
+	cout << "Option 2 : "<< status_string <<" by employee Name press keyboard 2" << endl;
+	int num;
+	checkIntInputType(&num, 1); // 1 is to search input data with 1 and 2
+	if (num == 1)
+	{
+		cout << "Please Enter employee ID" << endl;
+		checkIntInputType(&num);
+		if (emp_lvl->m_employee_id > 0)
+			{
+				for (int i = 0; i < emp_lvl->m_employee_id; i++)
+				{
+					if ((num - 1) == i)
+					{
+						if (deleteStatus == 1) // if == 1 delete the employee by emp_id
+						{
+							for (int j = i ;j < emp_lvl->m_employee_id; j++)
+							{
+								emp_lvl->personArray[j] = emp_lvl->personArray[j + 1];
+							}
+
+							emp_lvl->m_employee_id--;
+							cout << "Employee Deleted" << endl;
+						}
+						else
+						{
+							Boss* temp_person = EnterPosition(&emp_lvl->personArray[i].m_position);
+							cout << "Employee ID :" << i + 1 << "\tName : " << emp_lvl->personArray[i].m_name << "\t\tPosition : ";
+							temp_person->duty();
+							cout << endl;
+							delete temp_person;
+							temp_person = NULL;
+						}
+					}				
+				}
+			}
+			else 
+			{
+				cout << "Data is empty" << endl;
+			}
+			system("pause");
+			num = 0;
+	}
+
+	if (num == 2)
+	{
+		cout << "Please Enter employee Name" << endl;
+		string e_name;
+		cin >> e_name;
+		if (emp_lvl->m_employee_id > 0)
+		{
+			for (int i = 0; i < emp_lvl->m_employee_id; i++)
+			{
+				if (e_name == emp_lvl->personArray[i].m_name)
+				{
+					if (deleteStatus == 1) // if == 1 delete the employee by emp_id
+					{
+						for (int j = i; j < emp_lvl->m_employee_id; j++)
+						{
+							emp_lvl->personArray[j] = emp_lvl->personArray[j + 1];
+						}
+
+						emp_lvl->m_employee_id--;
+						cout << "Employee Deleted" << endl;
+					}
+					else {
+						Boss* temp_person = EnterPosition(&emp_lvl->personArray[i].m_position);
+						cout << "Employee ID :" << i + 1 << "\tName : " << emp_lvl->personArray[i].m_name << "\t\tPosition : ";
+						temp_person->duty();
+						cout << endl;
+						delete temp_person;
+						temp_person = NULL;
+					}
+					
+				}
+			}
+		}
+		else
+		{
+			cout << "No person found" << endl;
+		}
+		system("pause");
+	}
+	
+}
+
+//delete
+void DeleteEmployee(Employee_level* emp) {
+	SearchEmployee(emp, 1); //1 is turn on delete status
+}
+
+
+//Extra Func
+void checkIntInputType(int * num, int search) {
+	if (search == 1)
+	{
+		while (!(cin >> *num) && *num != 1 && *num != 2) {
+			cout << "\nEnter Number Again With 1 or 2" << endl;
+			cout << "Error : Enter only number" << endl;;
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		}
+	}
+	else
+	{
+		while (!(cin >> *num)) {
+			cout << "\nEnter Number Again" << endl;
+			cout << "Error : Enter only number" << endl;;
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		}
+	}
+	
+}
+
+Boss * EnterPosition(int * position) {
+	Boss* temp_person;
+	if (*position == 0)
+	{
+		temp_person = new Boss;
+		return temp_person;
+	}
+	else if (*position == 1) {
+		temp_person = new Manager;
+		return temp_person;
+	}
+	else {
+		temp_person = new Employee;
+		return temp_person;
 	}
 }
--- classConstructor/employee.h
+++ classConstructor/employee.h
@@ -1,38 +1,60 @@
 #pragma once
+#define MaxEmployee 1000
 #include <iostream>
 using namespace std;
 
-
-class boss {
+class Boss {
 public:
 	virtual void duty() {
 		cout << "Boss assigned tasks to manager" << endl;
 	}
+// 	virtual ~Boss() {
+// 		cout << "Boss Deconstrucot Called" << endl;
+// 	}
+	string m_name;
+	int m_position;
+		
+	
 };
 
-class manager : public boss {
+class Manager : public Boss {
 public:
 	void duty() {
 		cout << "Manager assigned tasks to employee, received tasks from boss" << endl;
 	}
+// 	~Manager() {
+// 		cout << "Manager Deconstrucot Called" << endl;
+// 	}
 };
 
-class employee : public boss {
+class Employee : public Boss {
 public:
 	void duty() {
 		cout << "Employee received tasks from manager" << endl;
 	}
+// 	~Employee() {
+// 		cout << "Employee Deconstrucot Called" << endl;
+// 	}
+};
+
+class Employee_level {
+public:
+	Boss personArray[MaxEmployee];
+	int m_employee_id = 0;
+
 
 };
 
 void ExitProgram(int * num);
+void AddNewEmployee(Employee_level * emp);
 void showMenu();
-void DisplayDetails();
-void DeleteEmployee();
-void EditEmployeeDetails();
-void SearchEmployee();
+void DisplayDetails(Employee_level * emp_lvl);
+void DeleteEmployee(Employee_level* emp);
+void EditEmployeeDetails(Employee_level * emp);
+void SearchEmployee(Employee_level * emp, int deleteStatus = 0);
 void ReOrdering();
 void DeleteAllData();
 
 //other tools
-int checkVarIsInt(int* numInt);//check for intType 1= true 0 = false
+void checkIntInputType(int* num, int search = 0);
+Boss* EnterPosition(int* position);
